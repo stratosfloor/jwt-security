e7df7a5d45a6b40d0dc137146878c8cebbbaaed6
Emil Norén <stratosfloor@hotmail.com>
2024-05-28 20:02:36 +0200

    Remove stuff

--- src/main/java/se/emilnoreen/jwt_security/JwtSecurityApplication.java
+++ src/main/java/se/emilnoreen/jwt_security/JwtSecurityApplication.java
@@ -8,8 +8,5 @@ public class JwtSecurityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JwtSecurityApplication.class, args);
-		System.out.println(System.getenv("POSTGRES_USERNAME"));
-		System.out.println(System.getenv("POSTGRES_PASSWORD"));
 	}
-	// e9f924ec-b113-47aa-857b-1d75453ec1a7
 }
